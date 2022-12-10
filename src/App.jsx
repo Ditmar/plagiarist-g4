1e760dc5b3c3095d5886b785c0c6d45f2686afa3
JuanPC06 <juancb628@gmail.com>
2022-12-10 05:55:52 +0000

    Creating branch ticket 0006 & creating LastDocument component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import {
   Route,
 } from 'react-router-dom';
 import Login from './pages/login';
-import Dashboard from './components/DashboardComponent/Dashboard';
-import UploadComponent from './components/UploadComponent';
-import LoaderComponent from './components/LoaderComponent';
-import LoaderSpinnerContextProvider from './context/LoaderSpinnerContextProvider';
-import Detail from './pages/detail';
+import LastComponent from './components/LastComponent/Index';
 function App() {
 
   const items = [
@@ -30,29 +26,7 @@ function App() {
   return (
     
       <Theme>
-        <LoaderSpinnerContextProvider>
-          <BrowserRouter>
-            <LoaderComponent />
-            <Routes>
-              <Route path="/" element={<>
-                <Dashboard> 
-                  <p>content </p> 
-                </Dashboard>
-              </>}/>
-              <Route path="/upload" element={<>
-                <Dashboard> 
-                  <UploadComponent />
-                </Dashboard>
-              </> } />
-              <Route path="/detail/:slug" element={<>
-                <Dashboard> 
-                  <Detail />
-                </Dashboard>
-              </> } />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </BrowserRouter>
-        </LoaderSpinnerContextProvider>
+        <LastComponent/>
       </Theme>
   );
 }
---
+++ src/components/LastComponent/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LastComponent = () => {
+  return (
+    <div>Last Documents</div>
+  )
+}
+
+export default LastComponent
