e71823d6ff2d0e2af687dcb49ad813053b49c327
Juan Daniel <sis331andsis312.juandaniel@gmail.com>
2022-12-15 12:54:28 -0400

    new changes 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Theme from "./theme/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
 import LastDocuments from "./components/LastComponent/LastDocuments";
 import MenuComponent from "./components/MenuComponent/MenuComponent";
+import Login from "./pages/login";
 
 function App() {
   const items = [
@@ -22,9 +22,7 @@ function App() {
 
   return (
     <Theme>
-      <MenuComponent />
-       
-      <LastDocuments />
+      
     </Theme>
   );
 }
