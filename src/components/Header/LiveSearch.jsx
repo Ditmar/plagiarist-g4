d90beb12d094640d687b721a3cdb8e4e5a0acc37
coroaide <opt004aydee@gmail.com>
2022-12-15 18:37:32 +0000

    resolving v3

--- src/components/Header/LiveSearch.jsx
+++ src/components/Header/LiveSearch.jsx
@@ -1,44 +1,25 @@
-import React from "react";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import * as React from "react";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
 import IconSearch from "../../assets/headerComponent/icon-search";
 const LiveSearch = () => {
   return (
-    <Stack sx={{ margin: "auto" }}>
-      <Autocomplete
-        size="small"
-        style={{ width: 413 }}
-        id="nba_player_demo"
-        popupIcon={<IconSearch />}
-        getOptionLabel={(jsonResults) =>
-          `${jsonResults.first_name} ${jsonResults.last_name}`
-        }
-        sx={{ width: 300 }}
-        isOptionEqualToValue={(option, value) =>
-          option.first_name === value.first_name
-        }
-        noOptionsText={"No Theses Available Yet"}
-        renderOption={(props, jsonResults) => (
-          <Box
-            sx={{
-              width: 500,
-              maxWidth: "100%",
-            }}
-            {...props}
-            key={jsonResults.id}
-          >
-            {jsonResults.first_name}
-            {jsonResults.last_name}
-          </Box>
-        )}
-        renderInput={(params) => (
-          <TextField {...params} label=" Search a Thesis" />
-        )}
-      />
-    </Stack>
+    <>
+      <Box size="small" style={{ width: 600 }}>
+        <TextField
+          size="small"
+          style={{ width: 600 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconSearch />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+    </>
   );
 };
-
 export default LiveSearch;
