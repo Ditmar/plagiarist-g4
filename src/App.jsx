6cb431118782f55e09fb6b2741109181ec7aa6e0
Juan Daniel <sis331andsis312.juandaniel@gmail.com>
2022-12-15 09:45:58 -0400

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,29 @@
-import Theme from './theme/theme';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Login from './pages/login';
-import Dashboard from './components/DashboardComponent/Dashboard';
-import UploadComponent from './components/UploadComponent';
-import LoaderComponent from './components/LoaderComponent';
-import LoaderSpinnerContextProvider from './context/LoaderSpinnerContextProvider';
-import Detail from './pages/detail';
-import './App.css';
-function App() {
+import Theme from "./theme/theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/login";
+import CardHome from './components/cardhome/CardHome';
+function App() {
   const items = [
     {
-      to: '/',
-      name: 'Home'  
+      to: "/",
+      name: "Home",
     },
     {
-      to: '/searchgiffs',
-      name: 'Search Memes'
+      to: "/searchgiffs",
+      name: "Search Memes",
     },
     {
-      to: '/login',
-      name: 'Login'
-    }
+      to: "/login",
+      name: "Login",
+    },
   ];
-  
 
   return (
-     
-      <Theme>
-        <LoaderSpinnerContextProvider>
-          <BrowserRouter>
-            <LoaderComponent />
-            <Routes>
-              <Route path="/" element={<>
-                <Dashboard> 
-                <p>context</p>
-                </Dashboard>
-              </>}/>
-              <Route path="/upload" element={<>
-                <Dashboard> 
-                  <UploadComponent />
-                </Dashboard>
-              </> } />
-              <Route path="/detail/:slug" element={<>
-                <Dashboard> 
-                  <Detail />
-                </Dashboard>
-              </> } />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </BrowserRouter>
-        </LoaderSpinnerContextProvider>
-      </Theme>
+    <Theme>
+      <CardHome />
+    </Theme>
   );
+}
 
-  }            
 export default App;
