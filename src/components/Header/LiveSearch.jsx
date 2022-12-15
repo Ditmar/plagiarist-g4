f2ab63bfdb9260b60eb7c32ed775e416323d5ab6
coroaide <opt004aydee@gmail.com>
2022-12-15 17:35:39 +0000

    resolving v2

--- src/components/Header/LiveSearch.jsx
+++ src/components/Header/LiveSearch.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import IconSearch from "../../assets/headerComponent/icon-search";
 const LiveSearch = () => {
-  const [jsonResults, setJsonResults] = useState([]);
-
-  useEffect(() => {
-    fetch("https://www.balldontlie.io/api/v1/players")
-      .then((response) => response.json())
-      .then((json) => setJsonResults(json.data));
-  }, []);
- 
   return (
     <Stack sx={{ margin: "auto" }}>
       <Autocomplete
@@ -24,7 +15,6 @@ const LiveSearch = () => {
         getOptionLabel={(jsonResults) =>
           `${jsonResults.first_name} ${jsonResults.last_name}`
         }
-        options={jsonResults}
         sx={{ width: 300 }}
         isOptionEqualToValue={(option, value) =>
           option.first_name === value.first_name
